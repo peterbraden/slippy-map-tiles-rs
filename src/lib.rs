add5d3964ef4b81124db9710aaa4bea9e1f74f39
Rory McCann <rory@technomancy.org>
2016-03-01 10:10:23 +0100

    tile stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,80 @@
+#[derive(PartialEq, Eq, Debug)]
+pub struct Tile {
+    zoom: u8,
+    x: u32,
+    y: u32,
+}
+
+impl Tile {
+    fn new(zoom: u8, x: u32, y: u32) -> Option<Tile> {
+        if x < 2^(zoom as u32) && y < 2^(zoom as u32) {
+            Some(Tile { zoom: zoom, x: x, y: y })
+        } else {
+            None
+        }
+    }
+
+    fn parent(&self) -> Option<Tile> {
+        match self.zoom {
+            0 => {
+                // zoom 0, no parent
+                None
+            },
+            _ => {
+                Tile::new(self.zoom-1, self.x/2, self.y/2)
+            }
+        }
+    }
+
+    fn subtiles(&self) -> Option<[Tile; 4]> {
+        match self.zoom {
+            std::u8::MAX => {
+                None
+            },
+            _ => {
+                let z = self.zoom+1;
+                let x = 2*self.x;
+                let y = 2*self.y;
+                Some([Tile{zoom:z, x:x, y:y}, Tile{zoom:z, x:x+1, y:y}, Tile{zoom:z, x:x, y:y+1}, Tile{zoom:z, x:x+1, y:y+1}])
+            }
+        }
+    }
+
+    fn centre_point(&self) -> LatLon {
+        let n: f32 = 2f32.powi(self.zoom as i32);
+        let lon_deg: f32 = (self.x as f32) / n * 360f32 - 180f32;
+        let lat_rad: f32 = ((1f32 - 2f32 * (self.y as f32) / n) * std::f32::consts::PI).sinh().atan();
+        let lat_deg: f32 = lat_rad * 180f32 * std::f32::consts::FRAC_1_PI;
+
+        // FIXME figure out the unwrapping here....
+        // Do we always know it's valid?
+        LatLon::new(lat_deg, lon_deg).unwrap()
+
+    }
+
+    fn center_point(&self) -> LatLon {
+        self.center_point()
+    }
+
+}
+
+#[derive(PartialEq, Debug)]
+pub struct LatLon {
+    lat: f32,
+    lon: f32,
+}
+
+impl LatLon {
+    fn new(lat: f32, lon: f32) -> Option<LatLon> {
+        if lat <= 90f32 && lat >= -90f32 && lon <= 180f32 && lon >= -180f32 {
+            Some(LatLon{ lat: lat, lon: lon })
+        } else {
+            None
+        }
+    }
+
+}
+
 pub fn xy_to_tc(x: u32, y: u32) -> [String; 6] {
     [
         format!("{:03}", x/1_000_000),
@@ -54,5 +131,31 @@ mod test {
         assert_eq!(res[5], "0004");
 
         assert_eq!(zxy_to_mp_path(2, 3, 4, "png"), "2/0000/0003/0000/0004.png".to_string());
+    }
+
+    fn tiles() {
+        let tile = Tile::new(1, 5, 5);
+        assert_eq!(tile.is_none(), true);
+
+        let tile = Tile::new(1, 0, 0);
+        assert_eq!(tile.is_none(), false);
+        let tile = tile.unwrap();
+        let parent = tile.parent();
+        assert_eq!(parent.is_none(), false);
+        let parent = parent.unwrap();
+        assert_eq!(parent, Tile::new(0, 0, 0).unwrap());
+
+        assert_eq!(parent.centre_point(), LatLon::new(0f32, 0f32).unwrap());
+
+        let children = parent.subtiles();
+        assert_eq!(children.is_none(), false);
+        let children: [Tile; 4] = children.unwrap();
+        assert_eq!(children[0], Tile::new(1, 0, 0).unwrap());
+        assert_eq!(children[1], Tile::new(1, 1, 0).unwrap());
+        assert_eq!(children[2], Tile::new(1, 0, 1).unwrap());
+        assert_eq!(children[3], Tile::new(1, 1, 1).unwrap());
+        
+
+
     }
 }
